4831e6c471707a0874ae5fd234834b2293c747d8
AlbabRiziq <albabriziq123@gmail.com>
2023-09-16 14:00:30 +0700

    update

---
+++ frontend/src/Route/UserComponents/Jurnal/Jurnal.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../../Components/Navbar/Navbar";
+import Navigation from "../../../Components/Navigation/Navigation";
+
+function Jurnal() {
+  return (
+    <div className="">
+      <Navbar />
+      <Navigation />
+    </div>
+  );
+}
+
+export default Jurnal;
